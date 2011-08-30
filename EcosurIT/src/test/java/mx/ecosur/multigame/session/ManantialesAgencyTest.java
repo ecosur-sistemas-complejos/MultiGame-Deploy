0ab50ebddeedd283cc1f532e0a9cf223d8865c95
Andrew Waterman <andrew.waterman@gmail.com>
2011-08-30 14:01:31 -0500

    Changes to integration tests to support JAAS.

--- EcosurIT/src/test/java/mx/ecosur/multigame/session/ManantialesAgencyTest.java
+++ EcosurIT/src/test/java/mx/ecosur/multigame/session/ManantialesAgencyTest.java
@@ -1,31 +1,28 @@
 package mx.ecosur.multigame.session;
 
-import com.sun.appserv.security.ProgrammaticLogin;
 import mx.ecosur.multigame.ejb.interfaces.RegistrarRemote;
 import mx.ecosur.multigame.ejb.interfaces.SharedBoardRemote;
 import mx.ecosur.multigame.enums.MoveStatus;
 import mx.ecosur.multigame.exception.InvalidMoveException;
-import mx.ecosur.multigame.exception.InvalidRegistrationException;
-import mx.ecosur.multigame.impl.Color;
+import mx.ecosur.multigame.grid.Color;
+import mx.ecosur.multigame.grid.entity.GameGrid;
+import mx.ecosur.multigame.grid.entity.GridPlayer;
+import mx.ecosur.multigame.grid.entity.GridRegistrant;
 import mx.ecosur.multigame.impl.entity.manantiales.*;
 import mx.ecosur.multigame.impl.enums.manantiales.AgentType;
 import mx.ecosur.multigame.impl.enums.manantiales.Mode;
 import mx.ecosur.multigame.impl.enums.manantiales.TokenType;
-import mx.ecosur.multigame.impl.model.GameGrid;
-import mx.ecosur.multigame.impl.model.GridPlayer;
-import mx.ecosur.multigame.impl.model.GridRegistrant;
-import mx.ecosur.multigame.model.Agent;
-import mx.ecosur.multigame.model.Game;
-import mx.ecosur.multigame.model.Move;
-import mx.ecosur.multigame.model.Registrant;
+import mx.ecosur.multigame.model.interfaces.Registrant;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.jms.JMSException;
+import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.security.auth.login.LoginContext;
 import java.rmi.RemoteException;
 import java.util.List;
+import java.util.Properties;
 
 import static org.junit.Assert.*;
 
@@ -44,45 +41,44 @@ public class ManantialesAgencyTest {
 
     private SimpleAgent[] agents;
 
-
     @Before
-    public void fixtures () throws RemoteException, NamingException, InvalidRegistrationException {
-        ProgrammaticLogin login = new ProgrammaticLogin();
-        login.login("MultiGame", "test");
-        InitialContext ic = new InitialContext();
+    public void fixtures () throws Exception {
+        Properties props = new Properties();
+        props.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+        props.put(Context.URL_PKG_PREFIXES, "org.jnp.interfaces.NamingContextFactory");
+        props.put(Context.PROVIDER_URL, "jnp://localhost:1099/");
+
+        AuthCallbackHandler cbHandler = new AuthCallbackHandler("alice", "test");
+        LoginContext lctx = new LoginContext("Client-MultiGame", cbHandler);
+        lctx.login();
+        InitialContext ic = new InitialContext(props);
 
         agents = new SimpleAgent [3];
-        
-        registrar = (RegistrarRemote) ic.lookup(
-            "mx.ecosur.multigame.ejb.interfaces.RegistrarRemote");
-
+        registrar = (RegistrarRemote) ic.lookup("mx.ecosur.multigame.ejb.interfaces.RegistrarRemote");
         ManantialesGame game = new ManantialesGame ();
         game.setMode(Mode.BASIC_PUZZLE);
-        Game boardGame = new Game (game);
-
-        Registrant registrant = registrar.register(new Registrant (new GridRegistrant ("alice")));
-        boardGame = registrar.registerPlayer(boardGame, registrant);
+        Registrant registrant = registrar.register(new GridRegistrant("alice"));
+        game = (ManantialesGame) registrar.registerPlayer(game, registrant);
 
         if (gameId == 0) {
-            gameId = boardGame.getId();
+            gameId = game.getId();
         }
 
         Color[] colors = { Color.BLUE, Color.RED, Color.PURPLE };
         for (int i = 0; i < 3; i++) {
-            registrar.registerAgent(boardGame, new Agent(new SimpleAgent(new GridRegistrant ("Agent-" + (i + 1)),
-                    colors[i], AgentType.SIMPLE)));
+            registrar.registerAgent(game, new SimpleAgent(new GridRegistrant ("Agent-" + (i + 1)),
+                    colors[i], AgentType.SIMPLE));
         }
 
         /* Get the SharedBoard */
-        board = (SharedBoardRemote) ic.lookup(
-                "mx.ecosur.multigame.ejb.interfaces.SharedBoardRemote");
-        game = (ManantialesGame) board.getGame(gameId).getImplementation();
+        board = (SharedBoardRemote) ic.lookup( "mx.ecosur.multigame.ejb.interfaces.SharedBoardRemote");
+        game = (ManantialesGame) board.getGame(gameId);
 
         /* Set the GamePlayers from the SharedBoard */
-        List<GridPlayer> players = game.getPlayers();
+        List<GridPlayer> players = (List<GridPlayer>) game.getPlayers();
         int counter = 0;
         for (GridPlayer p : players) {
-            if (p.getRegistrant().getName().equals("alice"))
+            if (p.getName().equals("alice"))
                 alice = (ManantialesPlayer) p;
             else if (p instanceof SimpleAgent) {
                 agents [ counter++ ] = (SimpleAgent) p;
@@ -94,21 +90,18 @@ public class ManantialesAgencyTest {
     }
 
     @Test
-       public void testBasicAgentMoves () throws InvalidMoveException, JMSException, InterruptedException {
+       public void testBasicAgentMoves () throws InvalidMoveException, InterruptedException {
            alice.setTurn (true);
 
            ManantialesFicha play = new ManantialesFicha(5, 4, alice.getColor(),
                            TokenType.MODERATE_PASTURE);
 
            ManantialesMove move = new ManantialesMove (alice, play);
-           Game game = board.getGame(gameId);
-           ManantialesGame currentGame = (ManantialesGame) game.getImplementation();
-           Move model = board.doMove(game, new Move(move));
-           move = (ManantialesMove) model.getImplementation();
+           ManantialesGame game = (ManantialesGame) board.getGame(gameId);
+           move = (ManantialesMove) board.doMove(game, move);
 
-           game = board.getGame(gameId);
-           currentGame = (ManantialesGame) game.getImplementation();
-           GameGrid grid = currentGame.getGrid();
+           game = (ManantialesGame) board.getGame(gameId);
+           GameGrid grid = game.getGrid();
 
            assertEquals (MoveStatus.EVALUATED, move.getStatus());
            assertEquals (play, grid.getLocation(play));
@@ -116,9 +109,8 @@ public class ManantialesAgencyTest {
            /* Wait for agents to move */
            Thread.sleep(15000);
 
-           game = board.getGame(gameId);
-           currentGame = (ManantialesGame) game.getImplementation();
-           grid = currentGame.getGrid();
+           game = (ManantialesGame) board.getGame(gameId);
+           grid = game.getGrid();
 
            assertEquals (4, grid.getCells().size());
        }
