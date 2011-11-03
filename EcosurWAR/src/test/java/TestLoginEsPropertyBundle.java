e3b14b17909934ab4937613495fb1e25a376ec27
Andrew Waterman <andrew.waterman@gmail.com>
2011-11-03 17:33:34 -0600

    Fix for MG-62
    Translations and support for English and Spanish resource bundles.

---
+++ EcosurWAR/src/test/java/TestLoginEsPropertyBundle.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ */
+public class TestLoginEsPropertyBundle {
+
+    private ResourceBundle bundle;
+
+    @Before
+    public void setUp() {
+        bundle = ResourceBundle.getBundle("login", new Locale("es"));
+    }
+
+    @Test
+    public void testTitle() {
+        assertEquals(bundle.getString("title"), "MultiGame a ChiapasGames");
+    }
+
+    @Test
+    public void  testBackGround() {
+        assertEquals(bundle.getString("background"), "ANTECEDENTES");
+    }
+
+}
